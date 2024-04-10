c4c2ece73be840650f925e5061a1f6e140254ac8
Craig <craig.mwangi@student.moringaschool.com>
2024-04-10 19:23:39 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+ 
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+
+    const listItem = document.createElement("li");
+
+   
+    listItem.textContent = taskDescription;
+
+    
+    taskList.appendChild(listItem);
+
+    // Clear the input field after adding the task
+    taskInput.value = "";
+
+    
+    taskInput.focus();
+  });
 });
